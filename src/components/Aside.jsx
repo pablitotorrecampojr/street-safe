6c9b50cf3ae288326ef88ec4ea805f15026011ef
Torrexx <fdc.pablitot@gmail.com>
2025-03-24 17:42:41 +0800

    sneat-ui: remove some elements

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -32,113 +32,37 @@ export default function Aside() {
         <div className="menu-inner-shadow"></div>
         <ul className="menu-inner py-1">
             <li className="menu-item active">
-            <a className="menu-link">
-                <i className="menu-icon tf-icons bx bx-home-circle"></i>
-                <div data-i18n="Analytics">Dashboard</div>
-            </a>
+              <a className="menu-link">
+                  <i className="menu-icon tf-icons bx bx-home-circle"></i>
+                  <div data-i18n="Analytics">Dashboard</div>
+              </a>
             </li>
-
-            <li className="menu-item">
-            <a className="menu-link menu-toggle">
-                <i className="menu-icon tf-icons bx bx-layout"></i>
-                <div data-i18n="Layouts">Layouts</div>
-            </a>
-
-            <ul className="menu-sub">
-                <li className="menu-item">
-                <a className="menu-link">
-                    <div data-i18n="Without menu">Without menu</div>
-                </a>
-                </li>
-                <li className="menu-item">
-                <a className="menu-link">
-                    <div data-i18n="Without navbar">Without navbar</div>
-                </a>
-                </li>
-                <li className="menu-item">
-                <a className="menu-link">
-                    <div data-i18n="Container">Container</div>
-                </a>
-                </li>
-                <li className="menu-item">
-                <a className="menu-link">
-                    <div data-i18n="Fluid">Fluid</div>
-                </a>
-                </li>
-                <li className="menu-item">
-                <a className="menu-link">
-                    <div data-i18n="Blank">Blank</div>
-                </a>
-                </li>
-            </ul>
-            </li>
-
             <li className="menu-header small text-uppercase">
-            <span className="menu-header-text">Pages</span>
+              <span className="menu-header-text">Pages</span>
             </li>
             <li className="menu-item">
-            <a className="menu-link menu-toggle">
-                <i className="menu-icon tf-icons bx bx-dock-top"></i>
-                <div data-i18n="Account Settings">Account Settings</div>
-            </a>
-            <ul className="menu-sub">
-                <li className="menu-item">
-                <a className="menu-link">
-                    <div data-i18n="Account">Account</div>
-                </a>
-                </li>
-                <li className="menu-item">
-                <a className="menu-link">
-                    <div data-i18n="Notifications">Notifications</div>
-                </a>
-                </li>
-                <li className="menu-item">
-                <a className="menu-link">
-                    <div data-i18n="Connections">Connections</div>
-                </a>
-                </li>
-            </ul>
+              <a className="menu-link">
+                  <i className="menu-icon tf-icons bx bx-layout"></i>
+                  <div data-i18n="Layouts">Layouts</div>
+              </a>
             </li>
             <li className="menu-item">
-            <a className="menu-link menu-toggle">
-                <i className="menu-icon tf-icons bx bx-lock-open-alt"></i>
-                <div data-i18n="Authentications">Authentications</div>
-            </a>
-            <ul className="menu-sub">
-                <li className="menu-item">
-                <a className="menu-link" target="_blank">
-                    <div data-i18n="Basic">Login</div>
-                </a>
-                </li>
-                <li className="menu-item">
-                <a className="menu-link" target="_blank">
-                    <div data-i18n="Basic">Register</div>
-                </a>
-                </li>
-                <li className="menu-item">
-                <a className="menu-link" target="_blank">
-                    <div data-i18n="Basic">Forgot Password</div>
-                </a>
-                </li>
-            </ul>
+              <a className="menu-link">
+                  <i className="menu-icon tf-icons bx bx-dock-top"></i>
+                  <div data-i18n="Account Settings">Account Settings</div>
+              </a>
             </li>
             <li className="menu-item">
-              <a className="menu-link menu-toggle">
+              <a className="menu-link">
+                  <i className="menu-icon tf-icons bx bx-lock-open-alt"></i>
+                  <div data-i18n="Authentications">Authentications</div>
+              </a>
+            </li>
+            <li className="menu-item">
+              <a className="menu-link ">
                 <i className="menu-icon tf-icons bx bx-cube-alt"></i>
                 <div data-i18n="Misc">Misc</div>
               </a>
-              <ul className="menu-sub">
-                  <li className="menu-item">
-                  <a className="menu-link">
-                      <div data-i18n="Error">Error</div>
-                  </a>
-                  </li>
-                  <li className="menu-item">
-                  <a className="menu-link">
-                      <div data-i18n="Under Maintenance">Under Maintenance</div>
-                  </a>
-                  </li>
-              </ul>
             </li>
         </ul>
       </aside>
