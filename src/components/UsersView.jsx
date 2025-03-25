a91ca837ec2fb0f1e296e740191b192b49a70d38
Torrexx <fdc.pablitot@gmail.com>
2025-03-25 16:05:12 +0800

    webapp-sub: refactor dynamic user view

--- src/components/UsersView.jsx
+++ src/components/UsersView.jsx
@@ -1,6 +1,5 @@
 import { auth, db } from "../firebase/firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDocs, collection } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as SolidIcons from "@fortawesome/free-solid-svg-icons";
@@ -13,29 +12,29 @@ export default function UsersView({ icon = "faUser", color = "primary", role = "
     const [userData, setUserData] = useState(null);
   
     useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
-        setUser(currentUser);
-  
-        if (currentUser) {
-          const userRef = doc(db, "users", currentUser.uid);
-          const userSnap = await getDoc(userRef);
-  
-          if (userSnap.exists()) {
-            setUserData(userSnap.data());
-          } else {
-            console.log("No user document found!");
-          }
-        }
-      });
-  
-      return () => unsubscribe();
+        const fetchUsers = async () => {
+            try {
+                const usersCollection = collection(db, "users");
+                const usersSnapshot = await getDocs(usersCollection);
+                const usersList = usersSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+                console.log(usersList);
+                setUserData(usersList);
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            }
+        };
+    
+    fetchUsers(); // Call function on mount
     }, []);
   
-    const selectedIcon =
-      SolidIcons[icon] || RegularIcons[icon] || BrandIcons[icon] || RegularIcons.faUser;
-  
+    const selectedIcon = SolidIcons[icon] || RegularIcons[icon] || BrandIcons[icon] || RegularIcons.faUser;
     const roleName = accountSetting.role[role] || "Unknown Role";
-
+    let filteredUsers = 0;
+    userData && userData.forEach((user) => { 
+        if (user.role === role) {
+            filteredUsers++;
+        }
+    });
     return (
       <div>
         <div className="card">
@@ -49,7 +48,7 @@ export default function UsersView({ icon = "faUser", color = "primary", role = "
               </div>
             </div>
             <h1 className="fw-semibold d-block mb-1">{roleName}</h1>
-            <h3 className="card-title mb-2">{userData ? Object.keys(userData).length : 0}</h3>
+            <h3 className="card-title mb-2">{ filteredUsers }</h3>
           </div>
         </div>
       </div>
