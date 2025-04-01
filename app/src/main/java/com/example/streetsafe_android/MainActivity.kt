282384a9c3ace4a051447da0829e09f15439810f
10969torrexx <pablitojrtorre@gmail.com>
2025-04-01 11:31:51 +0800

    android.v2: fragment navigation

--- app/src/main/java/com/example/streetsafe_android/MainActivity.kt
+++ app/src/main/java/com/example/streetsafe_android/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.example.streetsafe_android
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -20,7 +14,23 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!.findNavController()
-
         bottomNavView.setupWithNavController(navController)
+        bottomNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.profileFragment -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                R.id.settingsFragment -> {
+                    navController.navigate(R.id.settingsFragment)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
