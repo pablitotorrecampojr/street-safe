5b3dcb1e9269ba71b8967664d60058dfc49edf42
10969torrexx <pablitojrtorre@gmail.com>
2025-04-01 10:53:44 +0800

    android.v2: add fragmentations

--- app/src/main/java/com/example/streetsafe_android/MainActivity.kt
+++ app/src/main/java/com/example/streetsafe_android/MainActivity.kt
@@ -9,25 +9,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.sign_in)
+        setContentView(R.layout.activity_main)
 
-        // Ensure the ID exists in your XML, or replace it with the correct root layout ID
-        val rootView = findViewById<View>(android.R.id.content) // Fallback if R.id.main is missing
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!.findNavController()
 
-        val signupLink = findViewById<TextView>(R.id.signupLink)
-        signupLink.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
+        bottomNavView.setupWithNavController(navController)
     }
 }
