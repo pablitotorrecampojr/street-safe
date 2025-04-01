49fa8b8242cb93650cb435a76ddd5116b13d223f
10969torrexx <pablitojrtorre@gmail.com>
2025-04-02 00:40:29 +0800

    andoird.v2: create edit profile activity

--- app/src/main/java/com/example/streetsafe_android/EditProfile.kt
+++
@@ -1,4 +0,0 @@
-package com.example.streetsafe_android
-
-class EditProfile {
-}
--- app/src/main/java/com/example/streetsafe_android/ProfileFragment.kt
+++ app/src/main/java/com/example/streetsafe_android/ProfileFragment.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.streetsafe_android.databinding.FragmentProfileBinding
 
@@ -29,6 +27,11 @@ class ProfileFragment : Fragment() {
         //show sign in page
         binding.signOutButton.setOnClickListener {
             startActivity(Intent(requireContext(), SignInActivity::class.java))
+        }
+
+        //show edit profile page
+        binding.profileButton.setOnClickListener{
+            startActivity(Intent(requireContext(), EditProfileActivity::class.java))
         }
     }
 
