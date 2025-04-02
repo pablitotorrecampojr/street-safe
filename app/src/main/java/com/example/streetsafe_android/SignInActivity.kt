ea5760fc941f243678bdabafc94f7048fb4b2a23
10969torrexx <pablitojrtorre@gmail.com>
2025-04-02 23:27:25 +0800

    android.v2: initiate google sign in

--- app/src/main/java/com/example/streetsafe_android/SignInActivity.kt
+++ app/src/main/java/com/example/streetsafe_android/SignInActivity.kt
@@ -1,17 +1,26 @@
 package com.example.streetsafe_android
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
+    private lateinit var googleSignInClient : GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +37,12 @@ class SignInActivity : AppCompatActivity() {
         val signUpLink = findViewById<TextView>(R.id.signupLink)
         val googleSignInButton = findViewById<ImageView>(R.id.googleSignInButton)
         val facebookSignInButton = findViewById<ImageView>(R.id.facebookSignInButton)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         signInButton.setOnClickListener {
             val emailText = emailInput.text.toString().trim()
@@ -77,11 +92,50 @@ class SignInActivity : AppCompatActivity() {
 
         // Handle Google and Facebook Login (Not implemented yet)
         googleSignInButton.setOnClickListener {
-            Toast.makeText(this, "Google Sign-In coming soon!", Toast.LENGTH_SHORT).show()
+           signInGoogle()
         }
 
         facebookSignInButton.setOnClickListener {
             Toast.makeText(this, "Facebook Sign-In coming soon!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun signInGoogle(){
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result ->
+            if(result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(this, task.exception.toString() , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful){
+                val intent : Intent = Intent(this , MainActivity::class.java)
+                intent.putExtra("email" , account.email)
+                intent.putExtra("name" , account.displayName)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+            }
         }
     }
 }
