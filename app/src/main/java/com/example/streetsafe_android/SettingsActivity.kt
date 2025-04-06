0973298225250310828f4e6a6c662fb279eb1253
10969torrexx <pablitojrtorre@gmail.com>
2025-04-07 00:52:42 +0800

    android.v2: refactor settings page

--- app/src/main/java/com/example/streetsafe_android/SettingsActivity.kt
+++ app/src/main/java/com/example/streetsafe_android/SettingsActivity.kt
@@ -1,6 +1,11 @@
 package com.example.streetsafe_android
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity  : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.setting_activity)
+    }
 }
