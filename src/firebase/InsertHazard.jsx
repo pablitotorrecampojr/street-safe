4bf3b4177518f59f3d8e73cdcfda4124b84004d4
10969torrexx <pablitojrtorre@gmail.com>
2025-04-11 01:32:47 +0800

    webapp-sub: insert hazard --logs

--- src/firebase/InsertHazard.jsx
+++ src/firebase/InsertHazard.jsx
@@ -12,11 +12,11 @@ export default function ProtectedRoute({ children }) {
                 const newHazard = {
                     imageUrl: "base64Image",
                     dateSubmitted: "2025-04-11 00:00:00",
-                    fullAddress: "8WQ3+5W7, Camp Lapu-Lapu Rd, Lungsod ng Cebu, 6000 Lalawigan ng Cebu",
+                    fullAddress: "Cebu Transcentral Hwy, Cebu City, 6000 Cebu",
                     roadHazard: "Test Data",
                     status: 0,
-                    latitude: 10.337927,
-                    longitude: 123.904806,
+                    latitude: 10.3678299,
+                    longitude: 123.8682681,
                     userid: "vCdg9SpVlreJSEnHLY4De2HSW4F2"
                 };
 
