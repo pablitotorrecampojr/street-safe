fad4856dbff12d4fc5a6933100cea2214952c4d4
10969torrexx <pablitojrtorre@gmail.com>
2025-04-13 23:43:56 +0800

    webapp-sub: test insert data

--- src/firebase/InsertHazard.jsx
+++ src/firebase/InsertHazard.jsx
@@ -12,11 +12,11 @@ export default function ProtectedRoute({ children }) {
                 const newHazard = {
                     imageUrl: "base64Image",
                     dateSubmitted: "2025-04-11 00:00:00",
-                    fullAddress: "Banilad Rd, Cebu City, 6000 Cebu",
+                    fullAddress: "Turquoise St, Cebu City, 6000 Cebu",
                     roadHazard: "Test Data",
                     status: 0,
-                    latitude: 10.325945,
-                    longitude: 123.907919,
+                    latitude: 10.362220,
+                    longitude: 123.913778,
                     userid: "vCdg9SpVlreJSEnHLY4De2HSW4F2"
                 };
 
