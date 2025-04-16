94b1cc1d0100a3518e2733a6c0694a27f96ff6b7
Torrexx <fdc.pablitot@gmail.com>
2025-04-16 11:42:29 +0800

    webapp-sub: add new icons to status

--- src/webview/HazardFragment.jsx
+++ src/webview/HazardFragment.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { getDatabase, ref, onValue } from "firebase/database";
 import { hazard_status } from "../constants/hazard-report";
 import LoadingScreen from './LoadingScreen';
-import { hazard_icons } from '../constants/hazard-report';
+import { hazard_icons, hazard_color } from '../constants/hazard-report';
 
 export default function HazardFragment() {
   const location = useLocation();
@@ -85,7 +85,7 @@ export default function HazardFragment() {
                                 aria-expanded={isActive}
                                 onClick={() => handleAccordionClick(index)}
                               >
-                               <span className={`tf-icons bx ${hazard_icons[hazard.status]} mr-4`}></span> {hazard.roadHazard || `Hazard ${index + 1}`}
+                               <span className={`badge rounded-pill bg-label-${hazard_color[hazard.status]} mr-4`}><i className={hazard_icons[hazard.status]}></i></span> {hazard.roadHazard || `Hazard ${index + 1}`}
                               </button>
                             </h2>
 
