ec158834264a6080f1cb493b2820a7a2b49bc993
10969torrexx <pablitojrtorre@gmail.com>
2025-04-18 19:59:24 +0800

    android-sub.v2: add home fragment

--- app/src/main/java/com/example/streetsafe_android/HomeFragment.kt
+++ app/src/main/java/com/example/streetsafe_android/HomeFragment.kt
@@ -5,12 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val user = FirebaseAuth.getInstance().currentUser
+        val userId = user?.uid
+        val view = inflater.inflate(R.layout.fragment_maps, container, false)
+        val webView = view.findViewById<WebView>(R.id.mapsWebView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        val url = "${Constants.BASE_URL}home-fragment?userId=$userId"
+        webView.loadUrl(url)
+        return  view
     }
 }
