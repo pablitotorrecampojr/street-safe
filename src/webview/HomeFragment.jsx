fa1b370ba065f34a7afb6cbfc7818c8a816317fd
10969torrexx <pablitojrtorre@gmail.com>
2025-04-18 22:10:53 +0800

    webapp-sub: refactor components for home fragment

--- src/webview/HomeFragment.jsx
+++ src/webview/HomeFragment.jsx
@@ -85,30 +85,13 @@ export default function HomeFragment() {
                                             <div className="card-body">
                                             <div className="card-title d-flex align-items-start justify-content-between">
                                                 <div className="avatar flex-shrink-0">
-                                                <span className={`badge rounded-pill bg-label-info mr-4`}>
-                                                    <i className='bx bx-time-five'></i>
-                                                </span>
-                                                </div>
-                                                <div className="dropdown">
-                                                <button
-                                                    className="btn p-0"
-                                                    type="button"
-                                                    id="cardOpt3"
-                                                    data-bs-toggle="dropdown"
-                                                    aria-haspopup="true"
-                                                    aria-expanded="false"
-                                                >
-                                                    <i className="bx bx-dots-vertical-rounded"></i>
-                                                </button>
-                                                <div className="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
-                                                    <a className="dropdown-item" href="javascript:void(0);">View More</a>
-                                                    <a className="dropdown-item" href="javascript:void(0);">Delete</a>
-                                                </div>
+                                                    <span className={`badge rounded-pill bg-label-info mr-4`}>
+                                                        <i className='bx bx-time-five'></i>
+                                                    </span>
                                                 </div>
                                             </div>
-                                            <span className="fw-semibold d-block mb-1">Profit</span>
+                                            <span className="fw-semibold d-block mb-1">Pending</span>
                                                 <h3 className="card-title mb-2">$12,628</h3>
-                                                <small className="text-success fw-semibold"><i className="bx bx-up-arrow-alt"></i> +72.80%</small>
                                             </div>
                                         </div>
                                     </div>
@@ -121,26 +104,9 @@ export default function HomeFragment() {
                                                         <i className='bx bx-loader-circle'></i>
                                                     </span>
                                                 </div>
-                                                <div className="dropdown">
-                                                <button
-                                                    className="btn p-0"
-                                                    type="button"
-                                                    id="cardOpt3"
-                                                    data-bs-toggle="dropdown"
-                                                    aria-haspopup="true"
-                                                    aria-expanded="false"
-                                                >
-                                                    <i className="bx bx-dots-vertical-rounded"></i>
-                                                </button>
-                                                <div className="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
-                                                    <a className="dropdown-item" href="javascript:void(0);">View More</a>
-                                                    <a className="dropdown-item" href="javascript:void(0);">Delete</a>
-                                                </div>
-                                                </div>
                                             </div>
-                                            <span className="fw-semibold d-block mb-1">Profit</span>
+                                            <span className="fw-semibold d-block mb-1">In Progress</span>
                                                 <h3 className="card-title mb-2">$12,628</h3>
-                                                <small className="text-success fw-semibold"><i className="bx bx-up-arrow-alt"></i> +72.80%</small>
                                             </div>
                                         </div>
                                     </div>
@@ -153,26 +119,9 @@ export default function HomeFragment() {
                                                         <i className='bx bx-check-circle'></i>
                                                     </span>
                                                 </div>
-                                                <div className="dropdown">
-                                                <button
-                                                    className="btn p-0"
-                                                    type="button"
-                                                    id="cardOpt3"
-                                                    data-bs-toggle="dropdown"
-                                                    aria-haspopup="true"
-                                                    aria-expanded="false"
-                                                >
-                                                    <i className="bx bx-dots-vertical-rounded"></i>
-                                                </button>
-                                                <div className="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
-                                                    <a className="dropdown-item" href="javascript:void(0);">View More</a>
-                                                    <a className="dropdown-item" href="javascript:void(0);">Delete</a>
-                                                </div>
-                                                </div>
                                             </div>
-                                            <span className="fw-semibold d-block mb-1">Profit</span>
+                                            <span className="fw-semibold d-block mb-1">Resolved</span>
                                                 <h3 className="card-title mb-2">$12,628</h3>
-                                                <small className="text-success fw-semibold"><i className="bx bx-up-arrow-alt"></i> +72.80%</small>
                                             </div>
                                         </div>
                                     </div>
