a06487c260fc243d2c57ad67e8a5a23e0592978d
10969torrexx <pablitojrtorre@gmail.com>
2025-04-18 17:51:25 +0800

    webapp-sub: filter out hazards by account id

--- src/webview/HomeFragment.jsx
+++ src/webview/HomeFragment.jsx
@@ -20,8 +20,12 @@ export default function HomeFragment() {
             (snapshot) => {
             if (snapshot.exists()) {
                 const data = Object.values(snapshot.val());
-                const sortedDescending = data.sort(
-                (a, b) => new Date(b.dateSubmitted) - new Date(a.dateSubmitted)
+                const filteredHazards = data.filter(hazard => {
+                    const matchUser = String(hazard.userid) === String(userId);
+                    return matchUser;
+                })
+                const sortedDescending = filteredHazards.sort(
+                    (a, b) => new Date(b.dateSubmitted) - new Date(a.dateSubmitted)
                 );
                 setRoadHazards(sortedDescending);
             } else {
@@ -41,37 +45,42 @@ export default function HomeFragment() {
         <div className="layout-wrapper layout-content-navbar">
             <div className="layout-container">
                 <div className="layout-page">
-                <div className="content-wrapper">
-                    <div className="container-xxl flex-grow-1 container-p-y">
-                    <div className="row">
-                        <div className="col-md-6 mb-4 mb-md-0 mx-auto">
-                            <h1 className="text-left fw-bold">Street Safe</h1>
+                     {loading ? (
+                        <LoadingScreen loadingText="Fetching Map Data..." />
+                    ) : (
+                    <div className="content-wrapper">
+                        <div className="container-xxl flex-grow-1 container-p-y">
+                        <div className="row">
+                            <div className="col-md-6 mb-4 mb-md-0 mx-auto">
+                                <h1 className="text-left fw-bold">Street Safe</h1>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-8 mb-4 order-0">
-                            <div className="card">
-                                <div className="d-flex align-items-end row">
-                                <div className="col-sm-7">
-                                    <div className="card-body">
-                                    <h5 className="card-title text-primary">Welcome back 🎉</h5>
-                                    <p className="mb-4">
-                                        You have reported <span className="fw-bold">{roadHazards.length}</span> road hazards in total. Keep the community safe by reporting any road hazards you encounter.
-                                    </p>
+                        <div className="row">
+                            <div className="col-lg-8 mb-4 order-0">
+                                <div className="card">
+                                    <div className="d-flex align-items-end row">
+                                    <div className="col-sm-7">
+                                        <div className="card-body">
+                                        <h5 className="card-title text-primary">Welcome back 🎉</h5>
+                                        <p className="mb-4">
+                                            You have reported <span className="fw-bold">{roadHazards.length}</span> road hazards in total. Keep the community safe by reporting any road hazards you encounter.
+                                        </p>
 
-                                    <a href="javascript:;" className="btn btn-sm btn-outline-primary">Report Hazards</a>
+                                        <a href="javascript:;" className="btn btn-sm btn-outline-primary">Report Hazards</a>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-5 text-center text-sm-left">
+                                        <div className="card-body pb-0 px-0 px-md-4">
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="col-sm-5 text-center text-sm-left">
-                                    <div className="card-body pb-0 px-0 px-md-4">
-                                    </div>
-                                </div>
+                            </div>
                             </div>
                         </div>
                         </div>
                     </div>
-                    </div>
-                </div>
+                    )}
+                
                 </div>
             </div> 
         </div>
