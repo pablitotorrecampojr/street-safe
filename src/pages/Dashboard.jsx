7d478c0e4b36d05caebefd8993b3ccfa9d3f98e8
Torrexx <fdc.pablitot@gmail.com>
2025-04-24 17:14:52 +0800

    webapp-sub: minor fix on user view

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -20,31 +20,30 @@ const Dashboard = () => {
       <div className="layout-container">
           <Aside />
           <div className="layout-page">
-          <Navbar />
-
-          <div className='content-wrapper'>
-            <div className='container-xxl flex-grow-1 container-p-y'>
-              <div className='row'>
-                <div className="col-md-3 mb-4">
-                  <h1 style={{ fontSize: '20px' }} className='fw-bold'>Dashboard</h1>
+            <Navbar />
+            <div className='content-wrapper'>
+              <div className='container-xxl flex-grow-1 container-p-y'>
+                <div className='row'>
+                  <div className="col-md-3 mb-4">
+                    <h1 style={{ fontSize: '20px' }} className='fw-bold'>Dashboard</h1>
+                  </div>
                 </div>
-              </div>
-              <div className='row'>
-                <div className='col-md-3 mb-4'>
-                  <UsersView icon="faUser" color="success" role="0" />
-                </div>
-                <div className='col-md-3 mb-4'>
-                  <UsersView icon="faUsersGear" color="warning" role="1" />
-                </div>
-                <div className='col-md-3 mb-4'>
-                  <UsersView icon="faUsers" color="primary" role="2" />
-                </div>
-                <div className='col-md-3 mb-4'>
-                  <UsersView icon="faUserTie" color="danger" role="3" />
+                <div className='row'>
+                  <div className='col-md-3 mb-4'>
+                    <UsersView icon="faUser" color="success" role="0" />
+                  </div>
+                  <div className='col-md-3 mb-4'>
+                    <UsersView icon="faUsersGear" color="warning" role="1" />
+                  </div>
+                  <div className='col-md-3 mb-4'>
+                    <UsersView icon="faUsers" color="primary" role="2" />
+                  </div>
+                  <div className='col-md-3 mb-4'>
+                    <UsersView icon="faUserTie" color="danger" role="3" />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
           </div>
       </div>
       <div className="layout-overlay layout-menu-toggle" onClick={handleNavbarToggle}></div>
