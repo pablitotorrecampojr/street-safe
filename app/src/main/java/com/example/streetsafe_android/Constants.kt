926056690d567e613ab61830750cd142ef55b8de
Torrexx <83467428+10969torrexx@users.noreply.github.com>
2025-09-14 20:31:28 +0800

    reopen-app: changes in constant

--- app/src/main/java/com/example/streetsafe_android/Constants.kt
+++ app/src/main/java/com/example/streetsafe_android/Constants.kt
@@ -3,7 +3,7 @@ package com.example.streetsafe_android
 object Constants {
     const val IS_PRODUCTION = true
 
-    private const val DEV_BASE_URL  = "http://192.168.254.101:5173/"
+    private const val DEV_BASE_URL  = "http://192.168.254.111:5173/"
 //    private const val DEV_BASE_URL  = "http://192.168.7.70:5173/"
     private const val PROD_BASE_URL = "https://street-safe.vercel.app/"
 
