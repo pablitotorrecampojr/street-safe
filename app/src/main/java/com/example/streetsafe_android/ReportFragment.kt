c56532fa2efd6f13bf1644c3b79c602044ee66fd
Torrexx <83467428+10969torrexx@users.noreply.github.com>
2025-09-21 18:08:02 +0800

    reopen-app: changes to reporting hazard

--- app/src/main/java/com/example/streetsafe_android/ReportFragment.kt
+++ app/src/main/java/com/example/streetsafe_android/ReportFragment.kt
@@ -261,8 +261,8 @@ class ReportFragment : Fragment() {
     }
 
     private fun sendPostRequest(image: String, onResult: (String?) -> Unit) {
-        //val url = "http://192.168.107.46:5000/detect"
-        val url = "https://street-safe.onrender.com/detect"
+        val url = "http://192.168.254.105:5000/detect"
+//        val url = "https://street-safe.onrender.com/detect"
         val json = """
         {
             "image": "$image"
